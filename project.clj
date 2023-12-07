84b09191f40d05ef45009a739f7603f301504ab7
Jenkins CI <ci@puppet.com>
2023-12-07 23:58:20 +0000

    Version 2.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def jetty-version "10.0.18")
 
-(defproject puppetlabs/pcp-client "2.0.1"
+(defproject puppetlabs/pcp-client "2.0.2-SNAPSHOT"
   :description "client library for PCP"
   :url "https://github.com/puppetlabs/clj-pcp-client"
   :license {:name "Apache License, Version 2.0"
